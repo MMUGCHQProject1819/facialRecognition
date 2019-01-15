92d31aa9495369a857b3b5a50a818a6f905101fd
Allerion2016 <44294557+Allerion2016@users.noreply.github.com>
2019-01-15 18:09:00 +0000

    Tidying up the files
    Split the two detection files into a eye detection and a face detection folder since they do different things. TODO combine those two files so they work on only one .py file

--- eyeDetection/04_06.py
+++ eyeDetection/04_06.py
@@ -10,14 +10,14 @@ gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 #Load the instructions
 path = "haarcascade_eye.xml"
 
-
 eye_cascade = cv2.CascadeClassifier(path)
 
 #Object = path.Detect multiple variants (make it gray, set the size of the detection, minimum detections that can be near it, make the size to be as small as 10,10 px.
 eyes = eye_cascade.detectMultiScale(gray, scaleFactor=1.02, minNeighbors=5, minSize=(10, 10))
+
 print(len(eyes))
 
-#Calculations to draw the circle
+#Calculations to draw the circle on eyes
 for(x, y, w, h) in eyes:
     xc = (x + x+w)/2
     yc = (y + y+h)/2
@@ -25,6 +25,7 @@ for(x, y, w, h) in eyes:
     radius = w/2
     #Drawing the circle
     cv2.circle(img, (int(xc), int(yc)), int(radius), (255, 0, 0), 2)
+
 #Display the image
 cv2.imshow("Eyes", img)
 
