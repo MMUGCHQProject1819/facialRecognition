f10870f5e63313e0feafe338a76870b9363827be
Jacob Wybrant <jacobwybrant@hotmail.co.uk>
2019-03-15 23:35:59 +0000

    added db script

---
+++ mysqlDB.sql
@@ -0,0 +1,19 @@
+CREATE TABLE `person` (
+  `pid` int(11) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `dob` date DEFAULT NULL,
+  `nationality` varchar(255) DEFAULT NULL,
+  `height` int(11) DEFAULT NULL,
+  `weight` int(11) DEFAULT NULL,
+  `hair_colour` varchar(255) DEFAULT NULL,
+  `hair_style` varchar(255) DEFAULT NULL,
+  `skin_colour` varchar(255) DEFAULT NULL,
+  `facial_hair` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`pid`)
+);
+CREATE TABLE `photos` (
+  `pid` int(11) NOT NULL,
+  `image` mediumblob,
+  `encoding` text,
+  `encoded` int(1) DEFAULT NULL
+);1
